c286097b048e039138eb8599e8467fb79beb57cd
Mahmoud Reda Sharaf Eldeen <141069809+Sharaf-Eldeen@users.noreply.github.com>
2024-08-19 19:26:06 +0300

    Add Container Box and Adjust Layout

--- frontend/src/Card.jsx
+++ frontend/src/Card.jsx
@@ -4,6 +4,7 @@ import CardContent from "@mui/material/CardContent";
 import CardMedia from "@mui/material/CardMedia";
 import Typography from "@mui/material/Typography";
 import { useNavigate } from "react-router-dom";
+import Box from "@mui/material/Box";
 
 const EventCard = ({ slug, images, stadium, price }) => {
   const navigate = useNavigate();
@@ -13,29 +14,39 @@ const EventCard = ({ slug, images, stadium, price }) => {
   };
 
   return (
-    <Card
-      onClick={handleClick}
-      style={{
-        cursor: "pointer",
-        transition: "transform 0.2s",
-      }}
-      sx={{
-        "&:hover": {
-          transform: "scale(1.05)",
-        },
-      }}
-    >
-      <CardMedia
-        component="img"
-        height="140"
-        image={images[0]}
-        alt="Stadium Image"
-      />
-      <CardContent>
-        <Typography variant="h5">{stadium}</Typography>
-        <Typography variant="body1">Price: {price}</Typography>
-      </CardContent>
-    </Card>
+    <Box sx={{ margin: "1%", width: "100%", maxWidth: "90%" }}>
+      <Card
+        onClick={handleClick}
+        sx={{
+          width: "100%",
+          boxShadow: 10,
+          backgroundColor: "lightgray",
+          cursor: "pointer",
+          transition: "transform 0.2s",
+          "&:hover": {
+            transform: "scale(1.05)",
+          },
+        }}
+      >
+        <CardMedia
+          component="img"
+          sx={{
+            height: "220px",
+            width: "100%",
+          }}
+          image={images[0]}
+          alt="Stadium Image"
+        />
+        <CardContent>
+          <Typography variant="h5" color="text.secondary">
+            Stadium: {stadium}
+          </Typography>
+          <Typography variant="body1" color="text.secondary">
+            Price: {price}
+          </Typography>
+        </CardContent>
+      </Card>
+    </Box>
   );
 };
 
