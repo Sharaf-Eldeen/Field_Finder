4ddbaa3779c41fb1464956d9181bd8c651ee266b
Mahmoud Reda Sharaf Eldeen <141069809+Sharaf-Eldeen@users.noreply.github.com>
2024-08-19 20:44:23 +0300

    Basic Suggested Events Component Setup

---
+++ frontend/src/SuggestedEvents.jsx
@@ -0,0 +1,50 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+import Grid from "@mui/material/Grid";
+
+const numberOfSuggestedStadiums = 5;
+
+const SuggestedEvents = ({ city }) => {
+  const [suggestedEvents, setEvents] = useState([]);
+
+  useEffect(() => {
+    const fetchEvents = async () => {
+      try {
+        const response = await axios.get(`http://localhost:5500/api/stadiums`, {
+          params: {
+            city: city,
+            limit: numberOfSuggestedStadiums,
+          },
+        });
+
+        setEvents(response.data.stadiums);
+      } catch (error) {
+        console.error("Error fetching events: ", error);
+      }
+    };
+
+    fetchEvents();
+  }, [city]);
+
+  return (
+    <Box>
+      <Typography variant="h6" gutterBottom>
+        Suggested Stadiums
+      </Typography>
+      <Grid container spacing={2}>
+        {suggestedEvents.map((event) => (
+          <Grid item key={event.id}>
+            <Typography variant="h6">{event.name}</Typography>
+            <Typography variant="body2" color="text.secondary">
+              Price: {event.pricePerHour}
+            </Typography>
+          </Grid>
+        ))}
+      </Grid>
+    </Box>
+  );
+};
+
+export default SuggestedEvents;
