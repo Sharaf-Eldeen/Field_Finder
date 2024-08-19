3764623d48fe0131052aa2681fdd8ca9d924fa41
Mahmoud Reda Sharaf Eldeen <141069809+Sharaf-Eldeen@users.noreply.github.com>
2024-08-19 19:55:33 +0300

    Add Carousel for Images

--- frontend/src/CardDetail.jsx
+++ frontend/src/CardDetail.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { useLocation } from "react-router-dom";
 import Typography from "@mui/material/Typography";
+import { Carousel } from "react-responsive-carousel";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
 
 const EventDetail = () => {
   const { state } = useLocation();
@@ -16,6 +18,14 @@ const EventDetail = () => {
 
   return (
     <div>
+      <Carousel>
+        {images &&
+          images.map((img, index) => (
+            <div key={index}>
+              <img src={img} alt={`Event image ${index + 1}`} />
+            </div>
+          ))}
+      </Carousel>
       <Typography variant="h5">{stadium}</Typography>
       <Typography variant="body1">Location: {location}</Typography>
       <Typography variant="body1">Price: {price}</Typography>
