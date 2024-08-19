55cc15bbe95d1a5f269ca9d8a55e6f77637a92d6
Mahmoud Reda Sharaf Eldeen <141069809+Sharaf-Eldeen@users.noreply.github.com>
2024-08-19 19:24:39 +0300

    Add Image to EventCard Component

--- frontend/src/Card.jsx
+++ frontend/src/Card.jsx
@@ -4,9 +4,15 @@ import CardContent from "@mui/material/CardContent";
 import CardMedia from "@mui/material/CardMedia";
 import Typography from "@mui/material/Typography";
 
-const EventCard = ({ stadium, price }) => {
+const EventCard = ({ stadium, price, images }) => {
   return (
     <Card>
+      <CardMedia
+        component="img"
+        height="140"
+        image={images[0]}
+        alt="Stadium Image"
+      />
       <CardContent>
         <Typography variant="h5">{stadium}</Typography>
         <Typography variant="body1">Price: {price}</Typography>
