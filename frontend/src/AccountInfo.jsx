673077530e1a3c51bf8bd13e43eab7a42d31ad52
Mahmoud Reda Sharaf Eldeen <141069809+Sharaf-Eldeen@users.noreply.github.com>
2024-08-19 21:25:16 +0300

    Fix import statement for jwtDecode

--- frontend/src/AccountInfo.jsx
+++ frontend/src/AccountInfo.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
-import jwtDecode from "jwt-decode";
+import jwtDecode from "jwt-decode"; // Fixed import
 import { useNavigate } from "react-router-dom";
 import EventCard from "./EventCard";
 
