831fc8e189af2cfb5eea5db81cfac1b0604917f6
Mahmoud Reda Sharaf Eldeen <141069809+Sharaf-Eldeen@users.noreply.github.com>
2024-08-20 12:53:18 +0300

    Set up React Router and initial structure

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+function App() {
+  return (
+    <>
+      <Router>
+        <Routes>{/* Placeholder for routes */}</Routes>
+      </Router>
+    </>
+  );
+}
+
+export default App;
