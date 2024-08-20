d492d755012468db30e0b8c1836e310b40a6cf65
Mahmoud Reda Sharaf Eldeen <141069809+Sharaf-Eldeen@users.noreply.github.com>
2024-08-20 14:36:57 +0300

    Add Image Upload and Stadium Search

--- backend/routes/stadiums.js
+++ backend/routes/stadiums.js
@@ -2,12 +2,14 @@ const express = require("express");
 const { check, validationResult } = require("express-validator");
 const Stadium = require("../models/Stadium");
 const slugify = require("slugify");
+const uploader = require("../middlewares/uploadImages.js");
 
 const router = express.Router();
 
 // Create a new stadium
 router.post(
   "/",
+  uploader,
   [
     check("name", "Name is required").not().isEmpty(),
     check("city", "City is required").not().isEmpty(),
@@ -22,7 +24,10 @@ router.post(
     }
 
     try {
-      const { name, city, pricePerHour, ownerPhone, email } = req.body;
+      const { name, city, pricePerHour, ownerPhone, details, location, email } =
+        req.body;
+
+      const images = req.files.map((file) => file.filename) || [];
 
       const stadium = new Stadium({
         name,
@@ -30,6 +35,9 @@ router.post(
         pricePerHour,
         ownerPhone,
         slug: slugify(name, { lower: true, strict: true }),
+        details,
+        location: JSON.parse(location),
+        images,
         email,
       });
 
@@ -45,8 +53,38 @@ router.post(
 // Get all stadiums
 router.get("/", async (req, res) => {
   try {
-    const stadiums = await Stadium.find();
-    res.status(200).json(stadiums);
+    const { city = "", page = 1, limit = 10 } = req.query;
+
+    const pageNum = parseInt(page);
+    const limitNum = parseInt(limit);
+
+    const query = {
+      $or: [
+        { city: { $regex: city, $options: "i" } },
+        { name: { $regex: city, $options: "i" } }, // Use the same 'city' parameter to search in 'name'
+      ],
+    };
+
+    const options = {
+      sort: { price: 1 },
+      skip: (pageNum - 1) * limitNum,
+      limit: limitNum,
+    };
+
+    const stadiums = await Stadium.find(query, null, options);
+    const total = await Stadium.countDocuments(query);
+
+    const updatedStadiums = stadiums.map((stadium) => ({
+      ...stadium._doc,
+      images: stadium.images.map((image) => `http://localhost:5500/${image}`),
+    }));
+
+    res.status(200).json({
+      stadiums: updatedStadiums,
+      currentPage: pageNum,
+      totalPages: Math.ceil(total / limitNum),
+      totalItems: total,
+    });
   } catch (error) {
     res.status(500).json({ message: "Server error" });
   }
