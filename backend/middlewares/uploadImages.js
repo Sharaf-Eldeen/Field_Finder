5f2a26d9e7c5a6ad3a1da99c3f6ad21804578231
Mahmoud Reda Sharaf Eldeen <141069809+Sharaf-Eldeen@users.noreply.github.com>
2024-08-20 13:53:12 +0300

    Combine All Code into a Single Module

--- backend/middlewares/uploadImages.js
+++ backend/middlewares/uploadImages.js
@@ -1,6 +1,22 @@
 const multer = require("multer");
-const storage = require("./storageConfig");
-const checkFileType = require("./fileTypeValidator");
+const path = require("path");
+
+const storage = multer.diskStorage({
+  destination: path.join(__dirname, "../public/"),
+  filename: (req, file, cb) => {
+    cb(null, file.fieldname + "-" + Date.now() + "-" + file.originalname);
+  },
+});
+
+function checkFileType(file, cb) {
+  const allowedFileTypes = ["jpg", "jpeg", "png", "gif"];
+  const fileType = file.mimetype.split("/")[1];
+  if (allowedFileTypes.includes(fileType)) {
+    cb(null, true);
+  } else {
+    cb("Error: Images Only!");
+  }
+}
 
 const upload = multer({
   storage: storage,
