b4eac949947b5a2c76ec762056119a6fd0a883a5
Mahmoud Reda Sharaf Eldeen <141069809+Sharaf-Eldeen@users.noreply.github.com>
2024-08-20 12:55:05 +0300

    Add All routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,25 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import SearchAppBar from "./searchBar.jsx";
+import EventList from "./EventList";
+import EventDetail from "./CardDetail";
+import SignUp from "./SignUp.jsx";
+import SignIn from "./SignIn.jsx";
+import AccountInfo from "./AccountInfo.jsx";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
     <>
       <Router>
-        <Routes>{/* Placeholder for routes */}</Routes>
+        <Routes>
+          <Route path="/" element={<EventList />} />
+          <Route path="/events" element={<EventList />} />
+          <Route path="/stadium/:slug" element={<EventDetail />} />
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/signin" element={<SignIn />} />
+          <Route path="/account" element={<AccountInfo />} />
+        </Routes>
       </Router>
     </>
   );
