bb9f72cbca506d4268f5130f67fd360d9c0167c8
Mahmoud Reda Sharaf Eldeen <141069809+Sharaf-Eldeen@users.noreply.github.com>
2024-08-20 14:45:42 +0300

    Add CORS, Helmet, and Session Handling

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,31 @@
 const express = require("express");
+const cors = require("cors");
 const dotenv = require("dotenv");
 const path = require("path");
+const session = require("express-session");
+const helmet = require("helmet");
 
 dotenv.config();
 const app = express();
 
+app.use(cors());
+app.use(
+  session({
+    secret: process.env.cookieSignKey,
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: false },
+  })
+);
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(
+  helmet({
+    crossOriginResourcePolicy: { policy: "cross-origin" },
+    crossOriginOpenerPolicy: { policy: "same-origin-allow-popups" },
+  })
+);
 app.use(express.static(path.join(__dirname, "public")));
 
 const PORT = process.env.PORT || 5500;
