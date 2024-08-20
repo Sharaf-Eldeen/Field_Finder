587da8b7c26e631c1038488145558d494c348ece
Mahmoud Reda Sharaf Eldeen <141069809+Sharaf-Eldeen@users.noreply.github.com>
2024-08-20 13:58:48 +0300

    Add Slug Generation with Slugify

--- backend/models/Stadium.js
+++ backend/models/Stadium.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const slugify = require("slugify");
 
 const stadiumSchema = new mongoose.Schema({
   name: {
@@ -43,6 +44,13 @@ const stadiumSchema = new mongoose.Schema({
     type: String,
     required: true,
   },
+});
+
+stadiumSchema.pre("save", function (next) {
+  if (this.isModified("name")) {
+    this.slug = slugify(this.name, { lower: true, strict: true });
+  }
+  next();
 });
 
 const Stadium = mongoose.model("stadiums", stadiumSchema);
