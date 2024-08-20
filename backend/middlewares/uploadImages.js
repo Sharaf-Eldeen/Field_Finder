057f0befe87e2c9050d9eae47af909477d6788b6
Mahmoud Reda Sharaf Eldeen <141069809+Sharaf-Eldeen@users.noreply.github.com>
2024-08-20 13:52:05 +0300

    Add File Type Validation

--- backend/middlewares/uploadImages.js
+++ backend/middlewares/uploadImages.js
@@ -1,11 +1,11 @@
-const multer = require("multer");
-const path = require("path");
+function checkFileType(file, cb) {
+  const allowedFileTypes = ["jpg", "jpeg", "png", "gif"];
+  const fileType = file.mimetype.split("/")[1];
+  if (allowedFileTypes.includes(fileType)) {
+    cb(null, true);
+  } else {
+    cb("Error: Images Only!");
+  }
+}
 
-const storage = multer.diskStorage({
-  destination: path.join(__dirname, "../public/"),
-  filename: (req, file, cb) => {
-    cb(null, file.fieldname + "-" + Date.now() + "-" + file.originalname);
-  },
-});
-
-module.exports = storage;
+module.exports = checkFileType;
